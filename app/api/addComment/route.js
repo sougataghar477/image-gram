8c020414f1be940cc0903277265d448459d44518
sougata <sougataghar47@gmail.com>
2025-02-26 20:02:12 +0530

    first push

--- app/api/addComment/route.js
+++ app/api/addComment/route.js
@@ -2,27 +2,33 @@ import { v4 } from "uuid";
 import db from "@/utils/mongo";
 
 export async function POST(request) {
-  let commentId =v4().split("-")[0];
+  try {
+    let commentId = v4().split("-")[0];
     const body = await request.json();
-    console.log(body)
-    let user= await db.collection("users").findOneAndUpdate( { 
-          
-        "posts.id": body.postId
-      }, // Find user by username and specific post ID
-      { 
-        $addToSet: { "posts.$.comments": {
-          commentId:commentId,
-          comment: body.comment, 
-          commenter:body.commenter,
-          avatar:body.avatar,
-          likes:[],
-          replies:[],
-          commentCreatedAt:new Date()
-          } } 
+    console.log(body);
+
+    // Update the post with a new comment
+    const user = await db.collection("users").findOneAndUpdate(
+      { "posts.id": body.postId }, // Find the user with the post
+      {
+        $addToSet: {
+          "posts.$.comments": {
+            commentId,
+            comment: body.comment,
+            commenter: body.commenter,
+            avatar: body.avatar,
+            likes: [],
+            replies: [],
+            commentCreatedAt: new Date(),
+          },
+        },
       },
-      { returnDocument: "after" });
-      if(body.self===false){
-        await db.collection("notifications").findOneAndUpdate(
+      { returnDocument: "after" }
+    );
+
+    // If the post author is not the commenter, add a notification
+    if (body.self === false) {
+      await db.collection("notifications").findOneAndUpdate(
         { author: body.author },
         {
           $addToSet: {
@@ -31,15 +37,22 @@ export async function POST(request) {
               notifier: body.commenter,
               postId: body.postId,
               notificationType: "comment",
-               commentId:commentId,
+              commentId,
               createdAt: new Date(),
             },
           },
-
         },
         { returnDocument: "after" }
       );
     }
 
-  return Response.json({comments:user?.posts?.filter(post => post.id === body.postId)[0].comments})
+    // Ensure user exists before accessing properties
+    const comments =
+      user?.posts?.find((post) => post.id === body.postId)?.comments || [];
+
+    return Response.json({ comments });
+  } catch (error) {
+    console.error("Error handling POST request:", error);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
   }
+}
