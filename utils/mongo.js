a6f4868a20f435d2308be455637805f503755cf9
sougata <sougataghar47@gmail.com>
2025-02-26 21:42:56 +0530

    first push

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,6 +1,6 @@
  
 const {MongoClient} = require('mongodb');
-const client = new MongoClient(process.env.DB_URL);
+const client = new MongoClient("mongodb+srv://sougataghar47:sitonmeloba69@cluster0.fllgfxo.mongodb.net/plantgram?retryWrites=true&w=majority&appName=Cluster0");
 client.connect();
 let db = client.db("plantgram");
 module.exports = db;
