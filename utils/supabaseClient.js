4c473f76f8e1810313e7109904b3c0ecd8d131ea
sougata <sougataghar47@gmail.com>
2025-02-26 22:35:15 +0530

    second push

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://jrmntldlwbuaaufewyqk.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpybW50bGRsd2J1YWF1ZmV3eXFrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDU4ODY3MCwiZXhwIjoyMDU2MTY0NjcwfQ.nhxjeg25x1yrIShKuKgYsURFwIfDFCIXeSLFvBeJlkQ";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
