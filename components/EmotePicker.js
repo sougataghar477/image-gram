e385a32fcdb054f4c1a10aa7eaef45145c497fce
sougata <sougataghar47@gmail.com>
2025-03-01 10:15:38 +0530

    path

--- components/EmotePicker.js
+++ components/EmotePicker.js
@@ -1,6 +1,9 @@
 'use client';
+import { usePathname } from "next/navigation";
 import EmojiPicker from "emoji-picker-react";
 export default function EmotePicker({setPicker,setInputComment,fromCommentForm,setDescription,previewImage}){
+    const path=usePathname();
+    console.log(path)
     return <div className={`absolute  ${previewImage ? "bottom-0" : ""}`}>
         <EmojiPicker onEmojiClick={emoji => {
             setPicker(false);
